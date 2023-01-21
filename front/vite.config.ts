3d526744e83a38d61f82228a9f250755232ba83f
David Biró <dbiro97@gmail.com>
2023-01-21 04:51:16 +0100

    Add https for devserver

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,14 +1,44 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 import { VitePWA } from 'vite-plugin-pwa'
+import fs from 'fs'
+
+/**
+ * Vite clears the console after executing this file,
+ * but plugins can log messages
+ */
+const loggerPlugin = (function () {
+  const queue: unknown[] = []
+  const warn = (item: unknown) => queue.push(item)
+  const poll = () => {
+    // eslint-disable-next-line no-console
+    while (queue.length > 0) console.warn(queue.pop())
+  }
+  const transform = () => loggerPlugin.poll()
+  return { name: 'loggerPlugin', warn, poll, transform }
+})()
+
+
+let https = undefined
+try {
+  https = {
+    key: fs.readFileSync('../key.pem'),
+    cert: fs.readFileSync('../cert.pem'),
+  }
+} catch(e) {
+  loggerPlugin.warn("Please install a certificate, otherwise, you can't install the PWA");
+  loggerPlugin.warn("With HTTP you can only use this from the browser");
+}
+
+
 
 export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
+    https,
   },
-
   plugins: [
     react(),
     viteTsConfigPaths({
@@ -19,6 +49,7 @@ export default defineConfig({
       devOptions: {
         enabled: true
       }
-    })
+    }),
+    loggerPlugin
   ],
 });
