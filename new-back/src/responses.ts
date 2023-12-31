337c9a499b5554b5d48477eb89d3933a47ab0991
David Biró <dbiro97@gmail.com>
2023-12-31 11:15:27 +0100

    delete new-back

--- new-back/src/responses.ts
+++
@@ -1,7 +0,0 @@
-import { apiResponse } from '@cuple/server'
-
-export const unauthorizedError = <T,>(data: T) =>
-  apiResponse('unauthorized', 403, {
-    message: 'Unauthorized.',
-    ...data
-  })
