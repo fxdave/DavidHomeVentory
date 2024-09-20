cb8484d6fdebd125a4312616a9fa1210c1c17717
David Biró <dbiro97@gmail.com>
2024-09-20 21:03:03 +0200

    Save progress

--- front/src/services/useApi.tsx
+++ front/src/services/useApi.tsx
@@ -5,9 +5,97 @@ import {Routes} from "../../../back/src/index";
 
 export const token: string | undefined = undefined;
 
-const [useApiStore] = createStore<Client<Routes, Record<string, never>> | null>(
-  null,
-);
+type CupleClient = Client<Routes, Record<string, unknown>>;
+type CupleAuthedClient = Client<
+  Routes,
+  {
+    headers: {
+      Authorization: string;
+    };
+  }
+>;
+
+const [useApiStore] = createStore<CupleClient | null>(null);
+
+type Operation = {
+  type: "v1-create-item";
+  item: {
+    id: string | null;
+    parentId: string | null;
+    name: string;
+  };
+};
+
+const listOfOperations: Operation[] = [];
+
+interface WarehouseService {
+  create: CupleClient["warehouse"]["create"]["post"];
+  remove: CupleClient["warehouse"]["delete"]["delete"];
+  getOrCreate: CupleClient["warehouse"]["getOrCreate"]["post"];
+  list: CupleClient["warehouse"]["list"]["get"];
+  update: CupleClient["warehouse"]["update"]["put"];
+}
+
+function createOfflineWarehouseService(
+  _client: CupleAuthedClient,
+): WarehouseService {
+  return {
+    async create(props) {
+      listOfOperations.push({
+        type: "v1-create-item",
+        item: props.body,
+      });
+      return {
+        result: "success",
+        entry: {
+          id: props.body.id,
+        },
+      };
+    },
+
+    async remove(props) {
+      return await _client.warehouse.delete.delete(props);
+    },
+
+    async getOrCreate(props) {
+      return await _client.warehouse.getOrCreate.post(props);
+    },
+
+    async list(props) {
+      return await _client.warehouse.list.get(props);
+    },
+
+    async update(props) {
+      return await _client.warehouse.update.put(props);
+    },
+  };
+}
+
+function createOnlineWarehouseService(
+  _client: CupleAuthedClient,
+): WarehouseService {
+  return {
+    async create(props) {
+      return await _client.warehouse.create.post(props);
+    },
+
+    async remove(props) {
+      return await _client.warehouse.delete.delete(props);
+    },
+
+    async getOrCreate(props) {
+      return await _client.warehouse.getOrCreate.post(props);
+    },
+
+    async list(props) {
+      return await _client.warehouse.list.get(props);
+    },
+
+    async update(props) {
+      return await _client.warehouse.update.put(props);
+    },
+  };
+}
 
 export const useApi = () => {
   const [api, setApi] = useApiStore();
